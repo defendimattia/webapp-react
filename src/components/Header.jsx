500209756a3facadaf2655f8634a5399a43e70b5
defendimattia <defendi.mattia92@gmail.com>
2025-05-12 15:24:00 +0200

    add: bootstrap and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className="navbar navbar-expand-lg navbar-light bg-light px-4">
+            
+            <div className="logo">
+                LOGO
+            </div>
+
+            <nav className="ms-auto">
+                <NavLink to="/" className="nav-link text-secondary">Homepage</NavLink>
+            </nav>
+        </header>
+    );
+}
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,3 +1,8 @@
 export default function Book() {
-    return <h1>Specifiche Libro</h1>
+    return (
+        <>
+        <h1>Specifiche Libro</h1>
+        <h2>dettaglio film</h2>
+        </>
+    )
 }
