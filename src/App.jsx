97d45422ccaf62a6be60027a944df0eac3b1cba6
defendimattia <defendi.mattia92@gmail.com>
2025-05-13 10:39:34 +0200

    refactor: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainLayout />}>
+          <Route path="/" Component={MainLayout}>
             <Route index element={<Homepage />} />
             <Route path="/:id" element={<Movies />} />
           </Route>
